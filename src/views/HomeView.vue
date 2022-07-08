81fcd67aa26a6aee24827e84887d75e0e4c2ec91
juanfcolopez <jflopez2@uc.cl>
2022-07-08 12:25:59 -0400

    Add initial code

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <LandingMessage msg="Landing"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LandingMessage from '@/components/LandingMessage.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    LandingMessage
+  }
+}
+</script>
