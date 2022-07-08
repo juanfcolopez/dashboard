851b7e50a02040b2c27a30590f3cacbcdcdb7986
juanfcolopez <jflopez2@uc.cl>
2022-07-08 14:44:34 -0400

    Add router view to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,23 @@
 <template>
 <div>
-<div>
   <b-navbar toggleable="lg" type="dark" variant="primary">
-    <b-navbar-brand href="#">Dashboard</b-navbar-brand>
+    <b-navbar-brand>Dashboard</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
-      <!-- Right aligned nav items -->
+
       <b-navbar-nav class="ml-auto">
-        <b-nav-item href="/">Home</b-nav-item>
-        <b-nav-item href="/about">About</b-nav-item>
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
-</div>
+<router-view/>
 </div>
 </template>
 
