ed0a65f7e278bd3c74c1dff35f3d27d64ef15165
juanfcolopez <jflopez2@uc.cl>
2022-07-08 13:25:26 -0400

    Add basic navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="primary">
+    <b-navbar-brand href="#">Dashboard</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="/">Home</b-nav-item>
+        <b-nav-item href="/about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
