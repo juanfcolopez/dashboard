714f7fd459c06f1f642f5d6de451dbebe8aa191a
juanfcolopez <jflopez2@uc.cl>
2022-07-09 16:36:31 -0400

    Add products table component

---
+++ src/components/products/TableProducts.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div v-if="this.items.length === 0">
+      Cargando...
+    </div>
+    <div v-else>
+      <b-table striped hover
+        :items="items"
+        :fields="fields"
+        :sort-by="'Cantidad ($)'"
+        :sort-desc="true"
+      />
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'TableProducts',
+  props: {
+    items: []
+  },
+  data () {
+    return {
+      loading: true,
+      fields: [
+        {
+          key: 'Nombre',
+          sortable: true
+        },
+        {
+          key: 'Cantidad ($)',
+          sortable: true
+        }
+      ]
+    }
+  }
+}
+</script>
